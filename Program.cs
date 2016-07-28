2a6999ae6e24fe64073f83cb5bfd5246a336de5f
Fox-Alpha <Fox-Alpha@Tarnhoerner.de>
2016-07-28 15:34:58 +0200

    Add: Klasse für Konfiguration aus JSON Datei erstellt Add: Einlesen der der JSON Konfiguration Add: Funktion zum erstellen einer LogDatei Add: Schreiben einiger Informationen in Logdatei

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Diagnostics;
 
@@ -9,6 +10,7 @@ using Microsoft.Win32;
 
 using CommandLine;
 using CommandLine.Text;
+using Newtonsoft.Json;
 
 
 namespace checkInstalledSoftware
@@ -25,18 +27,48 @@ namespace checkInstalledSoftware
             Unknown = 3
         }
 
+        static string strJSONKonfigFile = "data\\settings.json";
+
         static int status = (int)nagiosStatus.Ok;
 
         static Dictionary<string, AppInformation> dicApplications;
 
         static string RegPath2Uninstall32 = "wow6432Node\\";
         static string RegPath2Uninstall = "Software\\{0}Microsoft\\Windows\\CurrentVersion\\Uninstall\\";
+
+        static Settings setting;
+
+        public static bool writeLog { get; private set; } = true;
         #endregion
 
         static int Main(string[] args)
         {
             dicApplications = new Dictionary<string, AppInformation>();
+            string[] cmdLine;
 
+            if ((cmdLine = Environment.GetCommandLineArgs()).Length > 1)
+            {
+                strJSONKonfigFile = cmdLine[1];
+            }
+
+            if (File.Exists(strJSONKonfigFile))
+            {
+                // Laden der Parameter aus der Konfiguration
+                ReadJSonKonfiguration(strJSONKonfigFile);
+                WriteToLogFile(string.Format("Start der Anwendung"), null);
+                WriteToLogFile("Speicherverbrauch: {0}", Environment.WorkingSet.ToString());
+
+                WriteToLogFile(string.Format("Laden der Such und Filter Bedingungen"), new string[] {strJSONKonfigFile});
+            }
+            else
+            {
+                Console.WriteLine("Es konnte keine Datei mit Einstellungen geladen werden|File=" + strJSONKonfigFile);
+                return (int)nagiosStatus.Warning;
+            }
+
+
+            WriteToLogFile("Lesen der Registry UNINSTALL Einträge", null);
+            WriteToLogFile("Speicherverbrauch: {0}", Environment.WorkingSet.ToString());
             if (Environment.Is64BitOperatingSystem)
             {
                 //  32 & 64 BIT Registry Bereiche lesen
@@ -49,9 +81,15 @@ namespace checkInstalledSoftware
                 GetRegistryInformation(false);
             }
 
+            WriteToLogFile("Exportieren der Daten", null);
+            WriteToLogFile("Speicherverbrauch: {0}", Environment.WorkingSet.ToString());
             ExportData();
+#if (DEBUG)
+            Console.WriteLine("Taste drücken zum fortsetzen ! ...");
+            Console.ReadKey();
+#endif
 
-            return (int)nagiosStatus.Unknown;
+            return (int)nagiosStatus.Ok;
         }
 
         static void GetRegistryInformation(bool is64Bit)
@@ -159,6 +197,74 @@ namespace checkInstalledSoftware
 
                     Debug.WriteLine(string.Format("{2} - {0} - {1}", ai.appName, ai.appVersion, strTemp));
                 }
+                WriteToLogFile("Es wurden {0} Einträge exportiert", dicApplications.Count.ToString());
+            }
+        }
+
+        static private void ReadJSonKonfiguration(string JSonFile)
+        {
+            if (!File.Exists(JSonFile))
+            {
+                Debug.WriteLine("Fehlende Angabe oder Angegebene Konfiguration ungültig.");
+                WriteToLogFile("Fehlende Angabe oder Angegebene Konfiguration ungültig.", "");
+                return;
+            }
+
+            //	Setzen der Serializer Settings
+            JsonSerializerSettings jsonSerializerSettings;
+
+            jsonSerializerSettings = new JsonSerializerSettings();
+            jsonSerializerSettings.Formatting = Formatting.Indented;
+            jsonSerializerSettings.MissingMemberHandling = MissingMemberHandling.Ignore;
+            jsonSerializerSettings.NullValueHandling = NullValueHandling.Include;
+            jsonSerializerSettings.StringEscapeHandling = StringEscapeHandling.EscapeNonAscii;
+            jsonSerializerSettings.TypeNameHandling = TypeNameHandling.Auto;
+
+            JsonSerializer serializer = JsonSerializer.CreateDefault(jsonSerializerSettings);
+
+            //StreamWriter sw = new StreamWriter (@"data\exampleOut.json");
+            //JsonWriter writer = new JsonTextWriter (sw);
+
+            using (StreamReader sr = new StreamReader(JSonFile))
+            {
+                using (JsonReader reader = new JsonTextReader(sr))
+                {
+                    setting = serializer.Deserialize<Settings>(reader);
+#if (DEBUG)
+                    string output = JsonConvert.SerializeObject(setting, jsonSerializerSettings);
+                    Debug.WriteLine(output);
+#endif
+                    if (!string.IsNullOrWhiteSpace(setting.strLogFile))
+                    {
+                        writeLog = true;
+                        WriteToLogFile("Start Operations", "");
+                    }
+                }
+            }
+        }
+
+        static void WriteToLogFile(string MessageFormat, params string[] vals)
+        {
+            if (!string.IsNullOrWhiteSpace(setting.strLogFile))
+            {
+                if (!setting.bAppend2Logfile)
+                {
+                    if (File.Exists(setting.strLogFile))
+                    {
+                        File.Delete(setting.strLogFile);
+                        setting.bAppend2Logfile = true;
+                    }
+                }
+
+                using (StreamWriter sw = File.AppendText(setting.strLogFile))
+                {
+                    if (vals != null && vals.Length > 0)
+                    {
+                        sw.Write(string.Format("{0}: {1}\r\n", DateTime.Now.ToString(), string.Format(MessageFormat, vals)));
+                    }
+                    else
+                        sw.Write(string.Format("{0}: {1}\r\n", DateTime.Now.ToString(), MessageFormat));
+                }
             }
         }
     }
@@ -256,5 +362,44 @@ namespace checkInstalledSoftware
         [Option('p', "pattern", Required = false, //Separator = ',',
         HelpText = "Welcher Begriff gesucht wird")]
         public string strFilter { get; set; }
+    }
+
+    [JsonObject(MemberSerialization.OptIn)]
+    class Settings
+    {
+        public Settings() { }
+
+        [JsonProperty(PropertyName = "Search", Required = Required.Always)]
+        public string strSearchTag { get; set; }
+
+        [JsonProperty(PropertyName = "Pattern", Required = Required.Always)]
+        public string strSearchPattern { get; set; }
+
+        [JsonProperty(PropertyName = "UseRegEx", Required = Required.Always)]
+        public bool bUseRegEx { get; set; } = false;
+
+        [JsonProperty(PropertyName = "CaseSensitive")]
+        public bool bCaseSensitive { get; set; } = false;
+
+        [JsonProperty(PropertyName = "ExportTargetDir")]
+        public string strExportTartgetDir { get; set; } = "";   // TODO: Verzeichnis der Anwendung benutzen
+
+        [JsonProperty(PropertyName = "ExportFileName", Required = Required.Always)]
+        public string strExportFileName { get; set; } = "ExportInstalledApplications";     //   TODO: Anwendungsungsname als Default
+
+        [JsonProperty(PropertyName = "ExportFileFormat", Required = Required.Always)]
+        public List<string> listFormat; // { get; set; } = new List<string>(){ "TXT" };
+
+        [JsonProperty(PropertyName = "Log", Required = Required.Always)]
+        public string strLogFile { get; set; } = "";
+
+        [JsonProperty(PropertyName = "AppendLog", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public bool bAppend2Logfile { get; set; } = false;
+
+        [JsonProperty(PropertyName = "LogSuffix", Required = Required.AllowNull)]
+        public string strLogSuffix { get; set; } = "";
+
+        [JsonProperty(PropertyName = "LogPrefix", Required = Required.AllowNull)]
+        public string strLogPrefix { get; set; } = "";
     }
 }
