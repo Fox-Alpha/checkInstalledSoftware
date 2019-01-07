c764a843aaa4375a4a5fd384c5629d28c05c8495
Dirk Buck <Dirk.buck@alpha-com.de>
2019-01-07 16:41:39 +0100

    Add: Hinweise für weitere Erweiterungen der Anwendung

--- Program.cs
+++ Program.cs
@@ -14,6 +14,23 @@ using CommandLine;
 using CommandLine.Text;
 using Newtonsoft.Json;
 
+/*
+	TODO:
+		- Durchsuchen von Pfaden im Dateisystem, zum Beispiel c:\Programme\[PATTERN]\*oder c:\programme (x86)\[PATTERN]\*
+		--	Durchsuvchen der Unterverzeichnisse zum Beispiel nach .exe Anwendungen
+		--- Auslesen der Dateiinformationen und abgleich mit den Registry Werten um Dubletten zu vermeiden
+		- Export als CSV datei
+		- Export in SQLite DB File
+		-- Verwendung von EntityFramework ???
+		-- Durchsuchen der SQLite nach einem [PATTERN]
+		+ AppInformation: Erweitern um das Feld, Installations Pfad
+		# Anwendung für "Man in the Middle" erstellen. ASP .NET Core unter Linux
+		-- Nimmt die Ergebnisse der Clients an und schreibt diese in die SQLite DB
+		-- Nimmt Anfragen aus dem Browser oder einer GUI Anwendung an um Ergebnisse Abzufragen
+		# Datenmodell:
+		--	Host, DateTime, Anwendungsname, Hersteller, Version, InstallPfad, Hashwert 
+		--- Hashwert aus Name, Hersteller, Version (evtl, Host) zur eindeutigen Identifizierung und Update
+*/
 
 namespace checkInstalledSoftware
 {
